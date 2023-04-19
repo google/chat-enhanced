aa8b0d0345479270fbe9336396e2c8bba0bdbf84
Antonio Lobato <aj.lobato@gmail.com>
2023-04-18 23:26:20 -0700

    Initial commit with working restored shortcut key.

---
+++ scripts/shortcut.js
@@ -0,0 +1,19 @@
+var button
+
+document.onkeydown = function(e) {
+  if (!e.ctrlKey) return
+
+  switch(e.key) {
+    case 'k':
+      console.log(button)
+      button.click()
+      e.preventDefault()
+      return
+  }
+}
+
+function updateButton() {
+  button = document.querySelector('[aria-label="Start a chat"]');
+}
+updateButton()
+setInterval(updateButton, 1000)
